5b90a1139dd55e8cd36bcde206f610301475fafb
sssho <43515414+sssho@users.noreply.github.com>
2018-09-30 17:30:06 +0900

    Change el-get initialization

--- .emacs.d/init-el-get.el
+++ .emacs.d/init-el-get.el
@@ -1,16 +1,11 @@
 (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 
 (unless (require 'el-get nil 'noerror)
-  (require 'package)
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.org/packages/"))
-  (package-refresh-contents)
-  (package-initialize)
-  (package-install 'el-get)
-  (require 'el-get))
-
-;;(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-;;(el-get 'sync)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 (el-get-bundle init-loader)
 

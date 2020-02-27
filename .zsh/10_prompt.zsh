2e642ef23f8a3141f7c86a518bd640ab29e4ec66
sssho <43515414+sssho@users.noreply.github.com>
2020-02-27 21:49:34 +0900

    Add vcs_info

--- .zsh/10_prompt.zsh
+++ .zsh/10_prompt.zsh
@@ -1,3 +1,18 @@
+autoload -Uz vcs_info
+
+precmd () {
+    [ -d .git ] || {
+        vcs_info_msg_0_=""
+        return 0
+    }
+    vcs_info
+}
+
+setopt prompt_subst
+RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
+
+zstyle ':vcs_info:*' formats "%F{green}[%b]%f"
+
 color_end="%{[0m%}"
 PROMPT="%(?.%{%F{yellow}%}.%{%F{red}%})%n@%m {%?} %~ ${color_end}
 %# "

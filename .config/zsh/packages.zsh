e4ad5077af11a8279ea26fa20b4b5216077272fa
sssho <43515414+sssho@users.noreply.github.com>
2020-08-23 20:08:37 +0900

    Fix zplug package list

--- .config/zsh/packages.zsh
+++ .config/zsh/packages.zsh
@@ -1,4 +1,4 @@
-zplug "$XDG_CONFIG_HOME"/zsh, from:local, use:"<->_*.zsh"
 zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-syntax-highlighting"
 zplug "mollifier/cd-gitroot"
+zplug "$XDG_CONFIG_HOME"/zsh, from:local, use:"<->_*.zsh"
